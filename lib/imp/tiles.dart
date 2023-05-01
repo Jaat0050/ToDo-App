39d03cf656470f04027eac8aa34c7248abb256f2
Jaat0050 <rana.arjun52000@gmail.com>
2023-05-01 17:39:23 +0530

    third  commit

--- lib/imp/tiles.dart
+++ lib/imp/tiles.dart
@@ -21,43 +21,61 @@ class Tiles extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(right: 25, left: 25, top: 25),
       child: Slidable(
-        endActionPane: ActionPane(
-          motion: const StretchMotion(), 
-          children: [
-            SlidableAction(
-              onPressed: deleteFunction,
-              icon: Icons.delete,
-              backgroundColor: Colors.red,
-              borderRadius: BorderRadius.circular(20),
-              )
-          ]
-          ),
+        endActionPane: ActionPane(motion: const StretchMotion(), children: [
+          SlidableAction(
+            onPressed: deleteFunction,
+            icon: Icons.delete,
+            backgroundColor: Colors.red,
+            borderRadius: BorderRadius.circular(10),
+          )
+        ]),
         child: Container(
-          padding: const EdgeInsets.all(10),
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(20),color: Colors.white,),
+          
+          padding: const EdgeInsets.all(0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+          ),
           child: Row(
             children: [
-              
-              //checkbox
-              Checkbox(
-                value: taskcompleted,
-                onChanged: onChanged,
-                activeColor: Colors.orange,
-                shape: const RoundedRectangleBorder(),
-              ),
-      
-              //task name
-              Text(
-                taskName,
-                style: TextStyle(
-                  decoration: taskcompleted ? TextDecoration.lineThrough : TextDecoration.none,
-                  fontSize: 20,
-                  color: Colors.deepOrangeAccent,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.italic
+
+              Expanded(
+                flex: 4,
+                child: Row(
+                  children: [
+                    //checkbox
+                    Checkbox(
+                      value: taskcompleted,
+                      onChanged: onChanged,
+                      activeColor: Colors.orange,
+                      shape: const RoundedRectangleBorder(),
+                    ),
+
+                    //task name
+                    Text(
+                      taskName,
+                      style: TextStyle(
+                          decoration: taskcompleted
+                              ? TextDecoration.lineThrough
+                              : TextDecoration.none,
+                          fontSize: 20,
+                          color: Colors.deepOrangeAccent,
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.italic),
+                    ),
+                  ],
                 ),
               ),
-      
+
+              //swipe and arrow row
+              Expanded(
+                child: Row(
+                  children: [
+                    Icon(Icons.keyboard_double_arrow_left_sharp),
+                    Text("Swipe")
+                  ],
+                ),
+              )
             ],
           ),
         ),
